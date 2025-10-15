aad4232dfbe486257c17aee48d34f63552cc8d58
AdrianaBartkova <adrianka.bartkova48@gmail.com>
2025-10-15 09:29:02 +0200

    refactor(Settings): update configuration settings and dependencies in config.py and pyproject.toml

--- config.py
+++ config.py
@@ -1,9 +1,8 @@
-from pydantic_settings import BaseSettings, Field, SettingsConfigDict
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    gemini_api_key: str = Field(..., env="GEMINI_API_KEY")
-
+    gemini_api_key: str = ""
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", extra="ignore"
     )
